f475bd3881f17e86dc077111d1f443240534b75f
chen <342467249@qq.com>
2019-02-24 15:00:41 +0800

    标准膳食摄入服务类

--- src/main/java/hdu/cn/hziee/HzieeApplication.java
+++ src/main/java/hdu/cn/hziee/HzieeApplication.java
@@ -1,8 +1,10 @@
 package hdu.cn.hziee;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan(basePackages = {"hdu.cn.hziee.dao"})
 @SpringBootApplication
 public class HzieeApplication {
 
